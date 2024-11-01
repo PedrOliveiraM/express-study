be19156de0250bda817777007ba1789b54817a85
PedrOliveiraM <pedropucmont@gmail.com>
2024-11-01 10:55:46 -0300

    feat: implement client management API with CRUD operations

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import router from './routes/route'
 
+dotenv.config()
+const app = express()
+const PORT = process.env.PORT
 
-import express from "express";
-
-const app = express();
-const PORT = process.env.PORT || 3000;
+app.use(express.json())
+app.use('/api', router)
 
 app.listen(PORT, () => {
-	console.log(`App is running port ${PORT}`);
-});
+	console.log(`Servidor executando na porta: ${PORT}`)
+})
